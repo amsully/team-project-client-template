89b54cd03eea46b0daeff007cf5baaf0cf04e840
jedesjardins <jedesjardins@umass.edu>
2016-11-14 16:17:14 -0500

    Installed react-router, started setting up links

--- app/app.js
+++ app/app.js
@@ -1,1 +1,27 @@
-/* Empty for now. :) */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainFeed from './components/mainfeed'
+import { IndexRoute, Router, Route, browserHistory } from 'react-router'
+
+class MainPage extends React.Component {
+  render() {
+    return <MainFeed/>;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>{this.props.children}</div>
+    )
+  }
+}
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      {/* Show the Feed at / */}
+      <IndexRoute component={MainPage} />
+    </Route>
+  </Router>
+),document.getElementById('center-feed'));
