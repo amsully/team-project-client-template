07273bde480393beb7c4e8daca5641028b3b93f5
vladislaveme <vemelyanov@umass.edu>
2016-11-15 03:08:06 +0000

    Added routing functionality, moved main page data to mainpage.js

--- app/app.js
+++ app/app.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainFeed from './components/mainfeed'
+import RightSideBar from './components/rightsidebar'
 import { IndexRoute, Router, Route, browserHistory } from 'react-router'
 
-class MainPage extends React.Component {
-  render() {
-    return(
-      <MainFeed />
-    );
-  }
-}
-
-class EmptyPage extends React.Component {
+class Test extends React.Component {
   render() {
     return (
       <div>
+        <h1>This is a test of the router</h1>
       </div>
-    );
+    )
   }
 }
 
@@ -24,7 +18,7 @@ class App extends React.Component {
   render() {
     return (
       <div>{this.props.children}</div>
-    )
+    );
   }
 }
 
@@ -32,8 +26,12 @@ ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       {/* Show the Feed at / */}
-      <IndexRoute component={MainPage} />
-      <Route path="" component={EmptyPage} />
+      <IndexRoute component={Test} />
+      <Route path="profile/" component={ProfilePage} />
+      <Route path="trip/1/" component={FullTripPage} />
+      <Route path="user-generated/" component={CustomizationPage} />
+      <Route path="customize/" component={Test} />
+
     </Route>
   </Router>
 ),document.getElementById("center-feed"));
