2647be343d14637e84563a19350bc120ac9e2e68
grahamhagenah <gwhagenah@gmail.com>
2016-11-16 12:27:07 -0500

    Adding simple customize.js and index.html modifications

--- app/app.js
+++ app/app.js
@@ -4,17 +4,8 @@ import MainPage from './components/mainpage';
 import UserGeneratedPage from './components/usergeneratedpage';
 import FullTripPage from './components/fulltrip';
 import RightSideBar from './components/rightsidebar'
+import Customize from './components/customize'
 import { IndexRoute, Router, Route, browserHistory } from 'react-router'
-
-class Test extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>This is a test of the router</h1>
-      </div>
-    )
-  }
-}
 
 class App extends React.Component {
   render() {
@@ -31,7 +22,7 @@ ReactDOM.render((
       <IndexRoute component={MainPage} />
 
       <Route path="user-generated/" component={UserGeneratedPage} />
-      <Route path="customize/" component={Test} />
+      <Route path="customize/" component={Customize} />
       <Route path="full-trip/" component={FullTripPage} />
       <Route path="rsb-test/" component={RightSideBar} />
     </Route>
