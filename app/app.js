08e8812ff0eb830894d06d650ca19ec83ee88cbf
vladislaveme <vemelyanov@umass.edu>
2016-11-16 19:27:03 +0000

    Side and nav bar suppport with router

--- app/app.js
+++ app/app.js
@@ -6,6 +6,41 @@ import FullTripPage from './components/fulltrip';
 import RightSideBar from './components/rightsidebar'
 import { IndexRoute, Router, Route, browserHistory } from 'react-router'
 
+class FullPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          
+        </nav>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-2">
+              <h1>Left Side Bar</h1>
+            </div>
+            <div className="col-md-7">
+              <Router history={browserHistory}>
+                <Route path="/" component={App}>
+                  {/* Show the Feed at / */}
+                  <IndexRoute component={MainPage} />
+
+                  <Route path="user-generated/" component={UserGeneratedPage} />
+                  <Route path="customize/" component={Test} />
+                  <Route path="full-trip/" component={FullTripPage} />
+                  <Route path="rsb-test/" component={RightSideBar} />
+                </Route>
+              </Router>
+            </div>
+            <div className="col-md-3">
+              <h1>Right Side Bar</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
 class Test extends React.Component {
   render() {
     return (
@@ -24,16 +59,4 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      {/* Show the Feed at / */}
-      <IndexRoute component={MainPage} />
-
-      <Route path="user-generated/" component={UserGeneratedPage} />
-      <Route path="customize/" component={Test} />
-      <Route path="full-trip/" component={FullTripPage} />
-      <Route path="rsb-test/" component={RightSideBar} />
-    </Route>
-  </Router>
-),document.getElementById("center-feed"));
+ReactDOM.render(<FullPage />,document.getElementById("full-page"));
