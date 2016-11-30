fd7375770d003ab5d978c517464a024b6617b272
amsully <asulliva@umass.edu>
2016-11-30 09:30:34 -0500

    Migrated Data to Mock Database on Server
    Kept the original Database on Client for everything else to keep working

--- server/src/database.js
+++ server/src/database.js
@@ -1,7 +1,187 @@
 // Your startup's initial mock objects go here
 var initialData = {
+  "users": {
+      "1": {
+        "_id": 1,
+        "FirstName": "Tim",
+        "LastName": "Richards",
+        "Username": "TRichards",
+        "Password": "*********",
+        "Email": "TRichards@umass.edu",
+        "Phone Number": "413-867-5309",
+        "Address": "54 Madison Avenue",
+        "feed": 6
+      }
+  },
 
+  "feeds": {
+    "rightSideBar": {
+      "_id": 1,
+      "contents":[]
+    },
+    "leftSideBar": {
+      "_id": 2,
+      "contents":[]
+    },
+    "customizationPage": {
+      "_id": 3,
+      "contents":[]
+    },
+    "profile": {
+      "_id": 4,
+      "contents":[]
+    },
+    "userGenerated": {
+      "_id": 5,
+      "contents":[]
+    },
+    "mainPage": {
+      "_id": 6,
+      "contents":[]
+    },
+    "fullTrip": {
+      "_id": 7,
+      "contents":[]
+    }
+  },
+
+  "feedItem": {
+    "1": {
+      "_id": 1,
+      "type": "ActivityItem",
+      "contents": {
+        "name":"New England Aquarium",
+        "type":"Aquarium",
+        "price":"$$$",
+        "rating":3
+      }
+    },
+    "2": {
+      "_id": 2,
+      "type": "AccommodationItem",
+      "contents": {
+        "type":"hotel",
+        "name":"The Westin Copley Place",
+        "price":"$299 per night",
+        "people":2
+      }
+    },
+    "3": {
+      "_id": 3,
+      "type": "RestaurantItem",
+      "contents": {
+        "type":"Seafood",
+        "name":"Neptune Oyster",
+        "price":"$$$",
+        "rating":4
+      }
+    },
+    "4": {
+      "_id": 4,
+      "type": "TripSummaryItem",
+      "trip_id":1,
+      "contents": {
+        "author":1,
+        "trip":1,
+        "start":"Amherst, MA",
+        "destination":"Boston, MA",
+        "dates":"6/24/2011 - 6/24/2011",
+        "summary":"A day trip to Boston by car for under $300. Destinations include the Boston Aquarium and Franklin Park Zoo. Planned restaurants include Italian Express Pizzeria and The Capital Grille."
+      }
+    },
+    "5": {
+      "_id": 5,
+      "type": "TripSummaryItem",
+      "trip_id":2,
+      "contents": {
+        "author":1,
+        "trip":2,
+        "start":"Boston, MA",
+        "destination":"Washington DC",
+        "dates":"2/4/2014 - 2/10/2014",
+        "summary":"A 7 day trip to Washington DC by airplane for under $9000. Destinations include the Museum of Natural History and the National Air and Space Museum. Planned restaurants include We the Pizza and GrillFish."
+      }
+    },
+    "6": {
+      "_id": 6,
+      "type": "TripSummaryItem",
+      "trip_id":3,
+      "contents": {
+        "author":1,
+        "trip":3,
+        "start":"Worcester, MA",
+        "destination":"Chatham, MA",
+        "dates":"3/29/2016 - 3/29/2016",
+        "summary":"A day trip to Cape Cod by car for under $500. Destinations include Nauset Beach and Chatham Lighthouse. Planned restaurants include Arnold's Restaurant and Longshore Restaurant."
+      }
+    },
+    "7": {
+      "_id": 7,
+      "type": "RestaurantItem",
+      "contents": {
+        "type":"American",
+        "name":"McDonalds",
+        "price":"$",
+        "rating":1
+      }
+    },
+    "8": {
+      "_id": 8,
+      "type": "RestaurantItem",
+      "contents": {
+        "type":"American",
+        "name":"Burger King",
+        "price":"$",
+        "rating":1
+      }
+    },
+    "9": {
+      "_id": 9,
+      "type": "ActivityItem",
+      "contents": {
+        "name":"Boston Common",
+        "type":"Parks and Recreation",
+        "price":"$",
+        "rating":4
+      }
+    },
+    "10": {
+      "_id": 10,
+      "type": "ActivityItem",
+      "contents": {
+        "name":"Fanuil Hall",
+        "type":"Historic Site",
+        "price":"$",
+        "rating":5
+      }
+    }
+  },
+
+  "trip":{
+    "1":{
+      "_id":1,
+      "author":1,
+      "accommodations":[2],
+      "restaurants":[3,7,8],
+      "activities":[1,9,10]
+    },
+    "2":{
+      "_id":2,
+      "author":1,
+      "accommodations":[],
+      "restaurants":[],
+      "activities":[]
+    },
+    "3":{
+      "_id":3,
+      "author":1,
+      "accommodations":[],
+      "restaurants":[],
+      "activities":[]
+      }
+    }
 };
+
 
 var data;
 // If 'true', the in-memory object representing the database has changed,
