bbe9154cda1d0ddf90c3efabcebd207eac18d903
amsully <asulliva@umass.edu>
2016-11-30 09:42:29 -0500

    Added '/full-trip/:tripid' endpoint. Added getFeedItemSync and getFullTripData methods
    Tested GET with Postman. Working

--- server/src/server.js
+++ server/src/server.js
@@ -21,6 +21,36 @@ app.use(bodyParser.text());
 // Support receiving JSON in HTTP request bodies
 app.use(bodyParser.json());
 
+function getFeedItemSync(feedItemId) {
+    var feedItem = database.readDocument('feedItem', feedItemId);
+
+    return feedItem;
+}
+
+function getFullTripData(trip) {
+    // Get the User object with the id "user".
+    var tripData = database.readDocument('trip', trip);
+
+    // Map the Feed's FeedItem references to actual FeedItem objects.
+    // Note: While map takes a callback function as an argument, it is
+    // synchronous, not asynchronous. It calls the callback immediately.
+    tripData.accommodations = tripData.accommodations.map(getFeedItemSync);
+    tripData.restaurants    = tripData.restaurants.map(getFeedItemSync);
+    tripData.activities     = tripData.activities.map(getFeedItemSync);
+    // Return FeedData with resolved references.
+    // emulateServerReturn will emulate an asynchronous server operation, which
+    // invokes (calls) the "cb" function some time in the future.
+    return tripData;
+}
+
+// Using '/full-trip' temporarily. Should switch to '/full-trip/:trip-id'
+app.get('/full-trip/:tripid', function(req, res) {
+  // URL parameters are stored in req.params
+  var tripid = req.params.tripid;
+  // Send response.
+  res.send(getFullTripData(tripid));
+});
+
 // Starts the server on port 3000!
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
